2fe9a4297d1a61deebe1855fab661a84219e2141
SAB <sevanboyadjian@gmail.com>
2022-04-06 19:59:57 -0400

    added dashboard

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -9,7 +9,7 @@ async function commentFormHandler(event) {
     ];
   
     if (comment_text) {
-      const response = await fetch('/api/comment', {
+      const response = await fetch('/api/comments', {
         method: 'POST',
         body: JSON.stringify({
           post_id,
