31c4c824323de90e37abb66f19ce638412963aa3
KW <kenneth_sl_wong@msn.com>
2022-04-07 16:48:15 -0400

    added profile section with route, added display of pets and pet form

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const profileRoutes = require('./profile-routes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
+router.use('/profile',profileRoutes)
 
 module.exports = router;
