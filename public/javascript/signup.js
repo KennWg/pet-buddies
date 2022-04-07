e4079f0b146d027bfbaff8780a9522055ecba8e9
KW <kenneth_sl_wong@msn.com>
2022-04-07 16:07:22 -0400

    added signup page to home route, fixed login and signup pages, removed delete/put routes for comments/posts, fixed styling for nav bar, added utilities for styling

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -4,14 +4,18 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-  
+    const address = document.querySelector('#address-signup').value.trim();
+    const city = document.querySelector('#city-signup').value.trim();
+
     if (username && email && password) {
       const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
           username,
           email,
-          password
+          password,
+          address,
+          city
         }),
         headers: { 'Content-Type': 'application/json' }
       });
